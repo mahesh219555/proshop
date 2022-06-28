06fd688389949adf4fc903bab2699ebc962cc455
mahesh219555 <108210803+mahesh219555@users.noreply.github.com>
2022-06-29 00:33:38 +0530

    Update ProductScreen.js

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -37,7 +37,7 @@ const ProductScreen = ({ history, match }) => {
       setRating(0)
       setComment('')
     }
-    if (!product._id || product._id !== match.params.id) {
+    if ( product._id !== match.params.id) {
       dispatch(listProductDetails(match.params.id))
       dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
     }
